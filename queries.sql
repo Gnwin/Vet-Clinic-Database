e31c1b7cf8bd4f9e393d6b4e32b85ac54306d2a2
Godwin Nwachukwu <nwgwin@gmail.com>
2022-06-14 16:44:18 +0100

    query animals table

--- queries.sql
+++ queries.sql
@@ -1,7 +1,22 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-createdb vet_clinic
-psql vet_clinic
+SELECT * FROM animals WHERE name LIKE '%mon';
 
--- SELECT * from animals WHERE name = 'Luna';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-30';
 
+SELECT name FROM animals WHERE neutered IS true AND escape_attempts < 3;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+
+
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pickachu';
+SELECT date_of_birth FROM animals WHERE name LIKE 'Agumon' OR name LIKE 'Pickachu';
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
+
+SELECT * FROM animals WHERE neutered IS true;
+
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE name <> 'Gabumon';
+SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
--- schema.sql
+++ schema.sql
@@ -1,12 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    id serial,
-    name varchar(25) NOT NULL,
-    date_of_birth date,
-    escape_attempts integer,
-    neutered boolean DEFAULT FALSE,
-    weight_kg decimal(4,2)
-);
+createdb vet_clinic
 
-CREATE TABLE;
+psql vet_clinic
+
+CREATE TABLE animals (
+id serial,
+name varchar(25) NOT NULL,
+date_of_birth date,
+escape_attempts integer,
+neutered boolean DEFAULT FALSE,
+weight_kg decimal(4,2)
+);
